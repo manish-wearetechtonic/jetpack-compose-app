bae823c6f6e433c7513d857787bb3ea855ba4647
thisismanishrajput <thisismanishrajput@gmail.com>
2024-03-14 22:19:46 +0530

    Remove the Login Screen bottom bar. Now the first screen will login screen if user is already logged in then only the home page will appear

--- app/src/main/java/com/example/myapplication/auth/screens/LandingPage.kt
+++ app/src/main/java/com/example/myapplication/auth/screens/LandingPage.kt
@@ -7,34 +7,33 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.example.myapplication.auth.AccessTokenValidator
+import com.example.myapplication.navigation.BottomNavigation
 import kotlinx.coroutines.launch
 
 @Composable
 fun LandingPage(navController: NavController) {
     val coroutineScope = rememberCoroutineScope()
     val context = LocalContext.current
+
+    val isLoggedIn = remember { mutableStateOf(false) }
+
     SideEffect {
         coroutineScope.launch {
-            val isLoggedIn = AccessTokenValidator.isValidToken(context)
-            if (isLoggedIn) {
-                navController.navigate(Screens.ScreensProductsRoute.route)
-            } else {
-                navController.navigate(Screens.ScreensLoginRoute.route)
-            }
+            isLoggedIn.value = AccessTokenValidator.isValidToken(context)
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Checking login status...")
+    if (isLoggedIn.value) {
+        BottomNavigation()
+    } else {
+        LoginScreen()
     }
 }
--- app/src/main/java/com/example/myapplication/navigation/BottomNavigation.kt
+++ app/src/main/java/com/example/myapplication/navigation/BottomNavigation.kt
@@ -1,39 +1,72 @@
 package com.example.myapplication.navigation
 
 import Screens
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
 import com.example.myapplication.auth.screens.LandingPage
 import com.example.myapplication.auth.screens.LoginScreen
+import com.example.myapplication.items
 import com.example.myapplication.presentation.chat.ChatScreen
+import com.example.myapplication.presentation.home.HomePage
 import com.example.myapplication.presentation.settings.SettingsScreen
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BottomNavigation(
-    modifier: Modifier,
-    bottomBarNavController: NavHostController
-) {
-    NavHost(
-        modifier = modifier,
-        navController = bottomBarNavController,
-        startDestination = Screens.ScreensLandingPageRoute.route
-    ) {
-        composable(route = Screens.ScreensLandingPageRoute.route){
-            LandingPage(navController = bottomBarNavController)
-        }
-        homeGraph(navController = bottomBarNavController)
-        composable(route = Screens.ScreensLoginRoute.route){
-            LoginScreen()
-        }
+fun BottomNavigation() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEach { item ->
+                    val isSelected = item.route == navController.currentDestination?.route
 
-        composable(route = Screens.ScreensChatRoute.route){
-            ChatScreen()
+                    NavigationBarItem(
+                        selected = isSelected,
+                        onClick = {
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                restoreState = true
+                                launchSingleTop = true
+                            }
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = if (isSelected) item.selectedIcon
+                                else item.unselectedIcon,
+                                contentDescription = item.title
+                            )
+                        },
+                        label = {
+                            Text(text = item.title)
+                        }
+                    )
+                }
+            }
         }
-        composable(route = Screens.ScreensSettingsRoute.route){
-            SettingsScreen()
+    ) {
+        NavHost(navController = navController, startDestination = Screens.ScreensHomeRoute.route) {
+            composable(Screens.ScreensHomeRoute.route) { HomePage(navController = navController) }
+            composable(Screens.ScreensChatRoute.route) { ChatScreen() }
+            composable(Screens.ScreensSettingsRoute.route) { SettingsScreen() }
         }
     }
 }
+
+
+
